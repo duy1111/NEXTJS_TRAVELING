ebb2c9033b90fb1f19347fadd7fdecb6e9181332
duy1111 <maixuanduy0605@gmail.com>
2023-06-13 00:25:49 +0700

    done base + ready deploy#6

--- next.config.js
+++ next.config.js
@@ -9,7 +9,17 @@ const nextConfig = {
             'avatars.githubusercontent.com',
             'lh3.googleusercontent.com'
         ]
-    }
+    },
+    "builds": [
+        {
+          "src": "build-command",
+          "use": "@vercel/static"
+        }
+      ],
+      "version": 2,
+      "env": {
+        "PRISMA_GENERATE_ON_BUILD": "true"
+      }
 }
 
 module.exports = nextConfig
